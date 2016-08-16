1c3d998cc7e2ee8cd8672470fc2313b5c963acab
Evgeniy Pristash <evgeniypristash@gmail.com>
2016-08-16 12:36:54 +0300

    Delete Exercise1.rb

--- Lesson 1/Exercise1.rb
+++
@@ -1,9 +0,0 @@
-puts "Как тебя зовут?"
-name = gets.chomp
-puts "Привет, #{name}, какой твой рост?"
-growth = gets.chomp
-perfect_weight = growth.to_i - 110
-if perfect_weight <= 0
-  puts "#{name}, твой вес уже оптимальный"
-  else puts "#{name}, твой вес должен быть #{perfect_weight}"
-end
