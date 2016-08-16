0afb59a3a88f34d23b069fb1fe8daefc3cb8d2e3
sumoist <evgeniypristash@gmail.com>
2016-08-16 12:17:30 +0300

    Lesson_1

--- Lesson_1/Exercise_4.rb
+++ Lesson_1/Exercise_4.rb
@@ -8,14 +8,13 @@ c = gets.chomp.to_f
 discriminant = b**2 - 4 * a * c
 
 if discriminant < 0
-  puts "Дискриминант равен #{discriminant}, корней нет"
-elsif discriminant > 0
-  x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
-  x2 = (-b - Math.sqrt(discriminant)) / (2 * a)
-  puts "Дискриминант равен #{discriminant}, корень #{x1}, корень #{x2}"
+  puts "Дискриминант = #{discriminant}, корней нет"
 else
-  x3 = (-b) / (2 * a)
-  puts "Дискриминант равен #{discriminant}, корень #{x3}"
+  x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
+  if discriminant > 0
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a)
+    puts "Дискриминант = #{discriminant}, корень №1 = #{x1}, корень №2 =  #{x2}"
+  else
+    puts "Дискриминант = #{discriminant}, корень №1 = корень №2 = #{x1}"
+  end
 end
-
-
