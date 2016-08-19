150a01232062b9e8bf7f539b07f829a0a81a595e
sumoist <evgeniypristash@gmail.com>
2016-08-20 01:03:53 +0300

    Fix Exercise 1, 3

--- Lesson_2/Exercise_1.rb
+++ Lesson_2/Exercise_1.rb
@@ -2,6 +2,4 @@ month_hash =
   {"январь" => 31, "февраль" => 29, "март" => 31, "апрель" => 30, "май" => 31, "июнь" => 30,
    "июль" => 31, "август" => 31, "сентябрь" => 30, "октябрь" => 31, "ноябрь" => 30, "декабрь" => 31}
 
-month_hash.each do |month, day|
-  puts month if day == 30
-end
+month_hash.each_pair {|month, day| puts month if day == 30}
--- Lesson_2/Exercise_3.rb
+++ Lesson_2/Exercise_3.rb
@@ -1,20 +1,9 @@
 arr = [0,1,1]
 
-first = 1
-second = 1
-seq = 2
-
 loop do
-  result = first + second
-  first = second
-  second = result
-  seq += 1
-
-  if result < 100
-    arr.push(result)
-  else
-    break
-  end
+  result = arr[-2] + arr[-1]
+  break if result > 100
+  arr.push(result)
 end
 
 puts "#{arr}"
